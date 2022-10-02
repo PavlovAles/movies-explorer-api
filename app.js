2ebdf6140378aa345713be517ea67ab46f20bd4e
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-10-02 13:21:53 +0600

    feat: add app; auth, handleError and logger middlewares

---
+++ app.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { errors } = require('celebrate');
+const router = require('./routes/index');
+const { handleError } = require('./middlewares/handleError');
+const { errorLogger, requestLogger } = require('./middlewares/logger');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/moviesDB');
+
+const corsOptions = {
+  origin: ['http://somedomain.com', 'https://somedomain.com', 'localhost:3001'],
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(requestLogger);
+
+app.use(router);
+app.use(errorLogger);
+app.use(errors());
+app.use(handleError);
+
+app.listen(PORT, () => { });

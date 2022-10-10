d05d7de68b60f099e4865a987cb2d333783eaed5
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-10-10 12:11:59 +0600

    feat: add erro messages

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -8,29 +8,52 @@ module.exports.idValidation = celebrate({
 
 module.exports.signupValidation = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
+    name: Joi.string().required().min(2).max(30)
+      .messages({
+        'any.required': 'Имя - обязательное поле',
+        'string.min': 'Длина имени должна быть от 2 до 30 символов',
+        'string.max': 'Длина имени должна быть от 2 до 30 символов',
+      }),
+    email: Joi.string().required().email().messages({
+      'any.required': 'Email - обязательное поле',
+      'string.email': 'Передан невалидный email',
+    }),
+    password: Joi.string().required().messages({
+      'any.required': 'Пароль - обязательное поле',
+    }),
   }),
 });
 
 module.exports.signinValidation = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
+    email: Joi.string().required().email().messages({
+      'any.required': 'Email - обязательное поле',
+      'string.email': 'Передан невалидный email',
+    }),
+    password: Joi.string().required().messages({
+      'any.required': 'Пароль - обязательное поле',
+    }),
   }),
 });
 
 module.exports.patchUserValidation = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    email: Joi.string().required().email(),
+    name: Joi.string().min(2).max(30)
+      .messages({
+        'any.required': 'Имя - обязательное поле',
+        'string.min': 'Длина имени должна быть от 2 до 30 символов',
+        'string.max': 'Длина имени должна быть от 2 до 30 символов',
+      }),
+    email: Joi.string().required().email().messages({
+      'any.required': 'Email - обязательное поле',
+      'string.email': 'Передан невалидный email',
+    }),
   }),
 });
 
 const regexURL = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/;
-const reqString = Joi.string().required();
-const reqNumber = Joi.number().required();
+const reqString = Joi.string().required().messages({ 'any.required': 'Не передан обязательный параметр' });
+const reqNumber = Joi.number().required().messages({ 'any.required': 'Не передан обязательный параметр' });
 const reqURL = Joi.string().required().regex(regexURL).message('Передана невалидная ссылка');
 
 module.exports.movieValidation = celebrate({

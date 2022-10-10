c16f769df6d910355af025b2d537b201e5500a32
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-10-10 09:59:49 +0600

    refactor: define named function findUserByCredentials

--- models/user.js
+++ models/user.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.statics.findUserByCredentials = function (email, password) {
+function findUserByCredentials(email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) throw new AuthenticationError('Неправильные почта или пароль');
@@ -33,6 +33,8 @@ userSchema.statics.findUserByCredentials = function (email, password) {
           return user;
         });
     });
-};
+}
+
+userSchema.statics.findUserByCredentials = findUserByCredentials;
 
 module.exports = mongoose.model('user', userSchema);

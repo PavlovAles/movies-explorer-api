e7009be0dfdcc65303bee93c2e59d9f528b88eb1
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-10-23 13:34:47 +0600

    fix: localhost at cors_origins

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,3 @@
-const CORS_ORIGINS = ['http://ales.movies.nomoredomains.icu/', 'https://ales.movies.nomoredomains.icu/', 'localhost:3001'];
+const CORS_ORIGINS = ['http://ales.movies.nomoredomains.icu/', 'https://ales.movies.nomoredomains.icu/', 'http://localhost:3001'];
 
 module.exports = { CORS_ORIGINS };
